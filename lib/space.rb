ac88d4df9e2cbb1c84eb52d001d8cd79b5ba53f0
keowashere <michaelkeohane@live.com>
2020-11-09 18:50:10 +0000

    space.create

--- lib/space.rb
+++ lib/space.rb
@@ -1,10 +1,28 @@
+require 'pg'
 class Space
 
-  def self.all
-    if RACK_ENV == 'test'
 
+
+  def self.all
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test') #name is slightly different
     else
-      
+      connection = PG.connect(dbname: 'MakersBnb')
+    end
+    result = connection.exec('SELECT * FROM spaces')
+    result.map {|space| space['name']}
+
+  end
+
+  def self.create(name:, owner:, availability:, description:, date:, price:)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test') #name is slightly different
+    else
+      connection = PG.connect(dbname: 'MakersBnb')
+    end
+
+    add_row_to_places_test_database
+
   end
 
 end
