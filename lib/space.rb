7689d1d375e187e0e40e32f2de829dbd7c9c2a56
Joseph <jpkershaw17@gmail.com>
2020-11-10 14:08:27 +0000

    .create test

--- lib/space.rb
+++ lib/space.rb
@@ -1,7 +1,17 @@
 require 'pg'
 class Space
 
+  attr_reader :id, :name, :owner, :availability, :description, :date, :price
 
+  def initialize(id:,name:, owner:, availability:, description:, date:, price:)
+    @id = id
+    @name = name
+    @owner = owner
+    @availability = availability
+    @description = description
+    @date = date
+    @price = price
+  end
 
   def self.all
     if ENV['RACK_ENV'] == 'test'
@@ -20,9 +30,9 @@ class Space
     else
       connection = PG.connect(dbname: 'MakersBnb')
     end
-
-    add_row_to_places_test_database
-
+    p "INSERT INTO spaces (name, owner, availability, description, date, price) VALUES('#{name}', '#{owner}', #{availability}, '#{description}', TO_DATE(#{date}, 'DD/MM/YYYY'), #{price}) RETURNING id, name, owner, availability, description, date, price ;"
+    result = connection.exec("INSERT INTO spaces (name, owner, availability, description, date, price) VALUES('#{name}', '#{owner}', #{availability}, '#{description}', TO_DATE('#{date}', 'DD/MM/YYYY'), #{price}) RETURNING id, name, owner, availability, description, date, price ;")
+     p Space.new(id: result[0]['id'] , name: result[0]['name'], owner: result[0]['owner'], availability: result[0]['availability'], description: result[0]['description'], date: result[0]['date'] , price: result[0]['price'] )
   end
 
 end
