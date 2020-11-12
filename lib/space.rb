9d7dc6370522efc8ffa908e589135ae3c28a4a97
Adam Baker <adamjamesbaker@gmail.com>
2020-11-12 16:43:05 +0000

    Request Front End

--- lib/space.rb
+++ lib/space.rb
@@ -15,6 +15,20 @@ class Space
     @price = price
   end
 
+  def self.find(number)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+     result = connection.exec("SELECT * FROM spaces WHERE id = #{number}")
+     
+     Space.new(id: result[0]['id'], name: result[0]['name'], owner: result[0]['owner'],
+      availability: result[0]['availability'], description: result[0]['description'],
+      date: result[0]['date'], price: result[0]['price'])
+  end
+
+
   def self.all
     if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test') #name is slightly different
