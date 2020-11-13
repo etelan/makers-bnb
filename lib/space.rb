317bcf196ebe1482f605a1909593ba45a3787819
Joseph <jpkershaw17@gmail.com>
2020-11-13 15:05:10 +0000

    allow ' to be used in comments

--- lib/space.rb
+++ lib/space.rb
@@ -59,6 +59,8 @@ class Space
     else
       connection = PG.connect(dbname: 'makersbnb')
     end
+    name = name.gsub("'"){"''"}
+    description = description.gsub("'"){"''"}
     result = connection.exec("INSERT INTO spaces (name, owner, availability, description, date, price, renter) VALUES('#{name}', '#{owner}', #{availability}, '#{description}', TO_DATE('#{date}', 'YYYY-MM-DD'), #{price}, '#{renter}') RETURNING id, name, owner, availability, description, date, price, renter ;")
     Space.new(id: result[0]['id'] , name: result[0]['name'], owner: result[0]['owner'], availability: result[0]['availability'], description: result[0]['description'], date: result[0]['date'] , price: result[0]['price'], renter: result[0]['renter'] )
 
